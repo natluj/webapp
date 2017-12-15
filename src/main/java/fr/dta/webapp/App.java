1bb5e9fdfb662d31f6f02c80848fb064ae426383
Julie TAN <jtan@DIGINAMIC.LOCAL>
2017-12-15 17:45:23 +0100

    webapp

---
+++ src/main/java/fr/dta/webapp/App.java
@@ -0,0 +1,12 @@
+package fr.dta.webapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("fr.dta")
+public class App {
+
+}
